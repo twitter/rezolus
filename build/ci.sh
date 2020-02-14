eac67e2a5456852e402b28f36c8c8e475e850e5a
Brian Martin <bmartin@twitter.com>
2020-02-14 10:42:45 -0800

    fix bash errors

--- build/ci.sh
+++ build/ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ev
 
 ## Install toolchain
-if ${TRAVIS_DIST} == "xenial"; then
+if [[ "${TRAVIS_DIST}" == "xenial" ]]; then
 	if ${LLVM_VERSION} = "8"; then
 		wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
         echo "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-8 main" | sudo tee -a /etc/apt/sources.list
@@ -10,7 +10,7 @@ if ${TRAVIS_DIST} == "xenial"; then
         sudo apt-get install linux-headers-`uname -r`
         sudo apt-get --yes install bison build-essential cmake flex git libclang-common-8-dev libelf-dev libllvm8 libz-dev lldb-8 llvm-8 llvm-8-dev llvm-8-runtime
 	fi
-	if ${LLVM_VERSION} = "9"; then
+	if [[ "${LLVM_VERSION}" == "9" ]]; then
 		wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
         echo "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-9 main" | sudo tee -a /etc/apt/sources.list
         sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/ppa
@@ -19,8 +19,8 @@ if ${TRAVIS_DIST} == "xenial"; then
         sudo apt-get --yes install bison build-essential cmake flex git libclang-common-9-dev libelf-dev libllvm9 libz-dev lldb-9 llvm-9 llvm-9-dev llvm-9-runtime
 	fi
 fi
-if ${TRAVIS_DIST} == "bionic"; then
-	if ${LLVM_VERSION} == "10"; then
+if [[ "${TRAVIS_DIST}" == "bionic" ]]; then
+	if [[ "${LLVM_VERSION}" == "10" ]]; then
 		wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
         echo "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-10 main" | sudo tee -a /etc/apt/sources.list
         sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/ppa
@@ -31,21 +31,21 @@ if ${TRAVIS_DIST} == "bionic"; then
 fi
 
 ## Optionally build/install BCC
-if [! -z "${BCC_VERSION}"]; then
+if [ ! -z "${BCC_VERSION}" ]; then
 	git clone https://github.com/iovisor/bcc || true
     cd bcc
     git checkout master
     git pull
-    if ${BCC_VERSION} == "0.10.0"; then
+    if [[ "${BCC_VERSION}" == "0.10.0" ]]; then
     	git checkout remotes/origin/tag_v0.10.0
     fi
-    if ${BCC_VERSION} == "0.11.0"; then
+    if [[ "${BCC_VERSION}" == "0.11.0" ]]; then
     	git checkout 0fa419a64e71984d42f107c210d3d3f0cc82d59a
     fi
-    if ${BCC_VERSION} == "0.12.0"; then
+    if [[ "${BCC_VERSION}" == "0.12.0" ]]; then
     	git checkout 368a5b0714961953f3e3f61607fa16cb71449c1b
     fi
-    if ${BCC_VERSION} == "latest"; then
+    if [[ "${BCC_VERSION}" == "latest" ]]; then
     	git checkout 368a5b0714961953f3e3f61607fa16cb71449c1b
     fi
     mkdir -p _build
@@ -57,7 +57,7 @@ if [! -z "${BCC_VERSION}"]; then
 fi
 
 ## Build and test
-if [-z "${FEATURES}"]; then
+if [ -z "${FEATURES}" ]; then
     cargo build
     cargo test
     cargo build --release
