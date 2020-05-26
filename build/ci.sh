c00859e22b909c5905398f60cbf2863b5d265148
Brian Martin <bmartin@twitter.com>
2020-05-26 16:46:24 -0700

    improve ci runtime (#103)
    Adds sccache to ci for both rust and bcc build. Reduces testing to just release mode binaries

--- build/ci.sh
+++ build/ci.sh
@@ -1,22 +1,41 @@
 #!/bin/bash -ev
 
-## Add LLVM repo
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
-echo "deb http://apt.llvm.org/${TRAVIS_DIST}/ llvm-toolchain-${TRAVIS_DIST}-${LLVM_VERSION} main" | sudo tee -a /etc/apt/sources.list
-sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/ppa
+set -e
 
-## Update apt
-sudo apt-get update
-
-## Install kernel headers for matching version
-sudo apt-get install linux-headers-"$(uname -r)"
-
-## Dist specific dependencies
-if [[ "${TRAVIS_DIST}" == "xenial" ]]; then
-    sudo apt-get --yes install bison build-essential cmake flex git libclang-common-"${LLVM_VERSION}"-dev libelf-dev libllvm"${LLVM_VERSION}" libz-dev lldb-"${LLVM_VERSION}" llvm-"${LLVM_VERSION}" llvm-"${LLVM_VERSION}"-dev llvm-"${LLVM_VERSION}"-runtime
+# try to install or use existing sccache
+if sccache --version > /dev/null 2>&1; then
+    echo "Using existing sccache"
+    export RUSTC_WRAPPER="sccache"
+    export CC="sccache gcc"
+    sccache --version
+elif cargo install sccache; then
+    echo "Installed sccache"
+    export RUSTC_WRAPPER="sccache"
+    export CC="sccache gcc"
+    sccache --version
+else
+    echo "Building without sccache"
 fi
-if [[ "${TRAVIS_DIST}" == "bionic" ]]; then
-    sudo apt-get --yes install bison build-essential cmake flex libfl-dev git libclang-common-"${LLVM_VERSION}"-dev libelf-dev libllvm"${LLVM_VERSION}" libz-dev lldb-"${LLVM_VERSION}" llvm-"${LLVM_VERSION}" llvm-"${LLVM_VERSION}"-dev llvm-"${LLVM_VERSION}"-runtime
+
+if [ -n "${LLVM_VERSION}" ]; then
+    ## Add LLVM repo
+    wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+    echo "deb http://apt.llvm.org/${TRAVIS_DIST}/ llvm-toolchain-${TRAVIS_DIST}-${LLVM_VERSION} main" | sudo tee -a /etc/apt/sources.list
+    sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/ppa
+
+    ## Update apt
+    sudo apt-get update
+
+    ## Install kernel headers for matching version
+    sudo apt-get install linux-headers-"$(uname -r)"
+
+    ## Dist specific dependencies
+    if [[ "${TRAVIS_DIST}" == "xenial" ]]; then
+        sudo apt-get --yes install bison build-essential cmake flex git libclang-common-"${LLVM_VERSION}"-dev libelf-dev libllvm"${LLVM_VERSION}" libz-dev lldb-"${LLVM_VERSION}" llvm-"${LLVM_VERSION}" llvm-"${LLVM_VERSION}"-dev llvm-"${LLVM_VERSION}"-runtime
+    fi
+    if [[ "${TRAVIS_DIST}" == "bionic" ]]; then
+        sudo apt-get --yes install bison build-essential cmake flex libfl-dev git libclang-common-"${LLVM_VERSION}"-dev libelf-dev libllvm"${LLVM_VERSION}" libz-dev lldb-"${LLVM_VERSION}" llvm-"${LLVM_VERSION}" llvm-"${LLVM_VERSION}"-dev llvm-"${LLVM_VERSION}"-runtime
+    fi
 fi
 
 ## Optionally build/install BCC
@@ -63,10 +82,6 @@ if [ -z "${TRAVIS_RUST_VERSION}" ]; then
     TRAVIS_RUST_VERSION="stable"
 fi
 
-cargo +${TRAVIS_RUST_VERSION} build --features ${FEATURES}
-cargo +${TRAVIS_RUST_VERSION} test --features ${FEATURES}
-sudo timeout --signal 15 --preserve-status 5.0m target/debug/rezolus --config configs/example.toml
-sudo timeout --signal 15 --preserve-status 5.0m target/debug/rezolus --config configs/ci.toml
 cargo +${TRAVIS_RUST_VERSION} build --release --features ${FEATURES}
 cargo +${TRAVIS_RUST_VERSION} test --release --features ${FEATURES}
 sudo timeout --signal 15 --preserve-status 5.0m target/release/rezolus --config configs/example.toml
