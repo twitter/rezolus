488c10bb78aa19753d5d339d45421cc2489f5596
Kyle Bartush <kbartush@twitter.com>
2021-06-04 09:18:28 -0700

    Adding license text to source

--- src/samplers/libcall/stat.rs
+++ src/samplers/libcall/stat.rs
@@ -1,3 +1,7 @@
+// Copyright 2019-2021 Twitter, Inc.
+// Licensed under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
 use rustcommon_metrics::{AtomicU32, AtomicU64, Source, Statistic};
 
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
