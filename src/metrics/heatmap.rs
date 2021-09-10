9192aa4f9e9ef2669ed7383d76cd18ceb33f47f1
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2021-09-10 13:32:20 -0700

    Add license headers

--- src/metrics/heatmap.rs
+++ src/metrics/heatmap.rs
@@ -1,3 +1,7 @@
+// Copyright 2019-2020 Twitter, Inc.
+// Licensed under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
 #![allow(dead_code)]
 
 use std::any::Any;
--- src/metrics/mod.rs
+++ src/metrics/mod.rs
@@ -1,3 +1,7 @@
+// Copyright 2019-2020 Twitter, Inc.
+// Licensed under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
 use std::any::Any;
 use std::ops::{Deref, DerefMut};
 use std::sync::atomic::{AtomicBool, Ordering};
