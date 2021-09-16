181e82d1a57d7f75118e53531ae22f7c3e15d707
Brian Martin <bmartin@twitter.com>
2021-09-16 13:39:56 -0700

    update dependencies (#242)
    Update dependencies prior to release.

--- build.rs
+++ build.rs
@@ -2,9 +2,8 @@
 // Licensed under the Apache License, Version 2.0
 // http://www.apache.org/licenses/LICENSE-2.0
 
-fn main() {
-    let mut flags = vergen::ConstantsFlags::all();
-    flags.toggle(vergen::ConstantsFlags::SEMVER_FROM_CARGO_PKG);
-    vergen::generate_cargo_keys(vergen::ConstantsFlags::all())
-        .expect("Unable to generate the cargo keys!");
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    vergen::vergen(vergen::Config::default())
 }
