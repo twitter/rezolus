580e3f3111b627bdc4c29e868928d16386287cba
Brian Martin <bmartin@twitter.com>
2021-11-24 14:21:50 -0800

    update to latest bcc crate

--- build/ci.sh
+++ build/ci.sh
@@ -79,45 +79,49 @@ git checkout master
 git pull
 if [[ "${BCC}" == "0.4.0" ]]; then
     git checkout remotes/origin/tag_v0.4.0
-fi
-if [[ "${BCC}" == "0.5.0" ]]; then
+elif [[ "${BCC}" == "0.5.0" ]]; then
     git checkout remotes/origin/tag_v0.5.0
-fi
-if [[ "${BCC}" == "0.6.0" ]]; then
+elif [[ "${BCC}" == "0.6.0" ]]; then
     git checkout remotes/origin/tag_v0.6.0
-fi
-if [[ "${BCC}" == "0.6.1" ]]; then
+elif [[ "${BCC}" == "0.6.1" ]]; then
     git checkout remotes/origin/tag_v0.6.1
-fi
-if [[ "${BCC}" == "0.7.0" ]]; then
+elif [[ "${BCC}" == "0.7.0" ]]; then
     git checkout remotes/origin/tag_v0.7.0
-fi
-if [[ "${BCC}" == "0.8.0" ]]; then
+elif [[ "${BCC}" == "0.8.0" ]]; then
     git checkout remotes/origin/tag_v0.8.0
-fi
-if [[ "${BCC}" == "0.9.0" ]]; then
+elif [[ "${BCC}" == "0.9.0" ]]; then
     git checkout remotes/origin/tag_v0.9.0
-fi
-if [[ "${BCC}" == "0.10.0" ]]; then
+elif [[ "${BCC}" == "0.10.0" ]]; then
     git checkout remotes/origin/tag_v0.10.0
-fi
-if [[ "${BCC}" == "0.11.0" ]]; then
+elif [[ "${BCC}" == "0.11.0" ]]; then
     git checkout 0fa419a64e71984d42f107c210d3d3f0cc82d59a
-fi
-if [[ "${BCC}" == "0.12.0" ]]; then
+elif [[ "${BCC}" == "0.12.0" ]]; then
     git checkout 368a5b0714961953f3e3f61607fa16cb71449c1b
-fi
-if [[ "${BCC}" == "0.13.0" ]]; then
+elif [[ "${BCC}" == "0.13.0" ]]; then
     git checkout 942227484d3207f6a42103674001ef01fb5335a0
-fi
-if [[ "${BCC}" == "0.14.0" ]]; then
+elif [[ "${BCC}" == "0.14.0" ]]; then
     git checkout ceb458d6a07a42d8d6d3c16a3b8e387b5131d610
-fi
-if [[ "${BCC}" == "0.15.0" ]]; then
+elif [[ "${BCC}" == "0.15.0" ]]; then
     git checkout e41f7a3be5c8114ef6a0990e50c2fbabea0e928e
-fi
-if [[ "${BCC}" == "0.16.0" ]]; then
+elif [[ "${BCC}" == "0.16.0" ]]; then
     git checkout fecd934a9c0ff581890d218ff6c5101694e9b326
+elif [[ "${BCC}" == "0.17.0" ]]; then
+    git checkout ad5b82a5196b222ed2cdc738d8444e8c9546a77f
+elif [[ "${BCC}" == "0.18.0" ]]; then
+    git checkout b1ab869032611d9fcdaea56851cd6126cca2eba8
+elif [[ "${BCC}" == "0.19.0" ]]; then
+    git checkout 4c561d037e2798563c2e87edcc5a406b020a458c
+elif [[ "${BCC}" == "0.20.0" ]]; then
+    git checkout 14278bf1a52dd76ff66eed02cc9db7c7ec240da6
+elif [[ "${BCC}" == "0.21.0" ]]; then
+    git checkout 321c9c979889abce48d0844b3d539ec9a01e6f3c
+elif [[ "${BCC}" == "0.22.0" ]]; then
+    git checkout 44fc17fc8ca0a53f37e82aa82a6a000ec28384c4
+elif [[ "${BCC}" == "0.23.0" ]]; then
+    git checkout 67f59ee80fcf5deedaacba1436d9fa09d32a16a0
+else
+    echo "unsupported bcc version: ${BCC}"
+    exit 1
 fi
 mkdir -p _build
 cd _build
